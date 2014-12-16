3df5d2290424756329878536e974ded8df421507
Jesus/John <Jesus/John@appacademy.io>
2014-12-16 10:40:28 -0500

    Finishes curry yum

--- curry.js
+++ curry.js
@@ -16,5 +16,17 @@ var curriedSum = function (numArgs) {
 
   return _curriedSum;
 };
-var sum = curriedSum(4);
- // => 56
+
+Function.prototype.curry = function(numArgs) {
+  var args = [];
+  var fn = this;
+  var _curry = function (arg) {
+    args.push(arg);
+    if (args.length === numArgs) {
+      return fn.apply(undefined, args);
+    } else {
+      return _curry;
+    }
+  }
+  return _curry;
+}
